10d401a3ac166be840d7c247709be55c6e4dc322
Mario Fronza <mariofronza1@gmail.com>
2024-03-06 22:01:14 -0300

    update script

--- config/script.sql
+++ config/script.sql
@@ -19,6 +19,42 @@ CREATE INDEX idx_client_id ON transactions (client_id);
 
 CREATE INDEX idx_transactions_by_client_and_created_at ON transactions (client_id, created_at DESC);
 
+CREATE OR REPLACE FUNCTION update_client_balance(
+    client_id_arg INT,
+    transaction_id_arg INT,
+    transaction_value_arg INT,
+    transaction_type_arg CHAR(1)
+) RETURNS VOID AS $$
+DECLARE
+    new_balance INT;
+    client_limit INT;
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM clients WHERE id = client_id_arg) THEN
+        RAISE EXCEPTION 'Client with ID % does not exist', client_id_arg;
+    END IF;
+
+    SELECT balance, lmt INTO new_balance, client_limit FROM clients WHERE id = client_id_arg;
+
+    IF transaction_type_arg = 'd' THEN
+        new_balance := new_balance - transaction_value_arg;
+    ELSE
+        new_balance := new_balance + transaction_value_arg;
+    END IF;
+
+    IF transaction_type_arg = 'd' AND new_balance < -client_limit THEN
+        RAISE EXCEPTION 'Transaction not allowed: Debit transaction exceeds client limit';
+    END IF;
+
+    UPDATE clients SET balance = new_balance WHERE id = client_id_arg;
+
+    INSERT INTO transactions (client_id, value, type, description)
+    VALUES (client_id_arg, transaction_value_arg, transaction_type_arg, 'Some description'); -- Adjust the description as needed
+
+    COMMIT;
+END;
+$$ LANGUAGE plpgsql;
+
+
 DO $$
 BEGIN
     INSERT INTO clients (name, lmt)
